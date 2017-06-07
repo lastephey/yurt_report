45260f5f3c9b266021ea1572a80fac8867810dfe
Laurie Stephey <lastephey@github.com>
2017-06-07 14:58:54 -0500

    added header, also installed twython

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,14 @@
+#! python
+
+from twython import Twython
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
 twitter = Twython(
     consumer_key,
     consumer_secret,
