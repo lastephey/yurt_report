d0e6f3390bba3d54c7f66baee2f8bd6a321742e1
Laurie Stephey <lastephey@github.com>
2017-06-11 17:37:05 -0500

    modified version of adafruit code to read from force sensor

---
+++ read_force_sensor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Based on code written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
+# modified by LS for yurt_report project
+
+import time
+import os
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+DEBUG = 1
+
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def get_force_signal(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
+
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+		#rename to be more clear
+		force_signal=adcout
+        return force_signal
+
