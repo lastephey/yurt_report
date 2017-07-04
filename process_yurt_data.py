fa57583a45a87c341a4001feb08659ca020838c1
Laurie Stephey <lastephey@github.com>
2017-07-04 13:48:59 -0500

    a fix to skip tweets which don't match the auto generated format

--- process_yurt_data.py
+++ process_yurt_data.py
@@ -13,15 +13,16 @@ import time
 import datetime
 from yurt_download import download_yurt_tweets
 
+#download the most recent data
+download_yurt_tweets()
+
 #number of tweets to analyze
 #288 tweets per day at one tweet every 5 min
 ndays=5
 tweets_per_day=288
 ntweets=ndays*tweets_per_day
-#just test that the basic functionality works first
-#download_yurt_tweets()
 
-#now reassemble the data line by line, the same way we wrote teh file
+#now reassemble the data line by line, the same way we wrote the file
 data = []
 with open('saved_yurt_tweets.json') as f:
     for line in f:
@@ -30,39 +31,37 @@ with open('saved_yurt_tweets.json') as f:
 text_data=[]		
 for i in range(0,len(data)):	
     text_data.append(data[i]["text"])
-	
-#print(text_data)
 
-cat_status=[]
 #now we need to parse the data and pull out the parts to use!
 #need timestamp and yurt status
-#only look at most recent 3k tweets becauase prior to that the format was different
 #first just create plot of timestamp vs yurt status (0 or 1)
+#need to sort which tweets we can actually use for analysis (exclude images and other tweets)
+analysis_array=[]
+cat_status=[]
 for i in range(0,ntweets):
     if "There" in text_data[i]:
-	    cat_status.append(1)
+        cat_status.append(1)
+        analysis_array.append(i)
     elif "Sorry" in text_data[i]:
         cat_status.append(0)
+        analysis_array.append(i)
     else:
-        print("error in string parse")
-		
-
+        continue
+        
 #now need to extract timestamps
 #timestamp always starts at the 18th character in both cases
 #timestamp always ends at the 44th character in both cases
-#only look at most recent 3k tweets becauase prior to that the format was different
 time_data=[]
 time_data_s=[]	
-for i in range(0,ntweets):
-    text_temp=text_data[i]	
+for i in range(0,len(analysis_array)):
+    j=analysis_array[i]
+    text_temp=text_data[j]	
     #this array is timestamp strings
     time_data.append(text_temp[18:44])
     #this converts the timestamp string to seconds
     time_data_s.append((datetime.datetime.strptime(text_temp[18:44], "%Y-%m-%d %H:%M:%S.%f")))
 	
-# The hour locator takes the hour or sequence of hours you want to
-# tick, not the base multiple
-
+# ok now plot
 fig, ax = plt.subplots()
 ax.plot_date(time_data_s, cat_status)
 ax.xaxis.set_major_locator(DayLocator())
