2fa3b00ca6e4734c924c1f5f051a0f9159ade106
Laurie Stephey <lastephey@github.com>
2017-07-06 09:38:59 -0500

    changing report time for test

--- tweet_yurt_data_daily.py
+++ tweet_yurt_data_daily.py
@@ -34,7 +34,7 @@ from auth import (
 
 #keep checking to see what time it is every 60 s
 #at some time every day, generate the last 24 hour report
-report_time='00:00:00';
+report_time='09:45:00';
 FMT='%H:%M:%S'
 print(flush=True)
 while True:
