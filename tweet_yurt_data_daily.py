2a2ad838f6a09c3b5cc43bc5c5f55627b298b77c
Laurie Stephey <lastephey@github.com>
2017-07-06 09:47:29 -0500

    need to change name of image to be tweeted

--- tweet_yurt_data_daily.py
+++ tweet_yurt_data_daily.py
@@ -124,7 +124,7 @@ while True:
 
         #and upload
         message=("Here is today's Yurt Report! %s" % now)
-        tweetpic = open("popular_yurt_times_daily.png","rb")
+        tweetpic = open("popular_yurt_times.png","rb")
         # Update status with our new image and status
         twitter.update_status_with_media(status=message, media=tweetpic)
         print("Tweeted: %s" % message)
